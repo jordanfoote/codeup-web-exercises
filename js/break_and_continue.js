a8a50c9afdcf48762d33c719168404c97e369fdc
Jordan Foote <jordan.foote@outlook.com>
2022-02-11 10:07:28 -0600

    Completed Break and Continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,11 +2,35 @@
 
 /** EXERCISE FROM JAVA CODEUP CURRICULUM (BREAK AND CONTINUE) **/
 
-var num = prompt("Please enter an odd number between 1 and 50.");
+var oddNumber = promptForNumber(1, 50);
+oddNumber = parseInt(oddNumber);
 
-for (var i = 0; i <= 50; i++) {
+printWithSkip(1, 50, oddNumber);
 
-    if (i % 2 === 1) {
-
+function printWithSkip(low, high, skipNumber) {
+    for (let i = low; i <= high; i++) {
+        if (i === skipNumber || i % 2 === 0) {
+            if (i === skipNumber) {
+                console.log("Yikes! Skipping number: " + i);
+            }
+            continue;
+        }
+        console.log("Here is an odd number: " + i);
     }
+}
+// console.log(oddNumber);
+
+function promptForNumber (low, high) {
+
+    var result;
+
+    while (true) {
+        var result = prompt("Please enter a number from " + low + " to " + high + ".");
+        // console.log(result);
+        if (result !== null && result % 2 === 1 && result >= low && result <= high) {
+            break;
+        }
+    }
+    // console.log(result);
+    return result;
 }
