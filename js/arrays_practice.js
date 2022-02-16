d8dd1606801c3547fdab0a27c26f6d589deca28c
Jordan Foote <jordan.foote@outlook.com>
2022-02-16 10:11:19 -0600

    Completed JS Objects Exercise

---
+++ js/arrays_practice.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/** EXERCISES FROM CLASS**/
+
+var fruits = [];
+
+fruits.push("apple");
+console.log(fruits);
+
+fruits.push("banana");
+console.log(fruits);
+
+fruits.push("cherry");
+console.log(fruits);
+
+fruits.push("lemon");
+console.log(fruits);
+
+fruits.push("mango");
+console.log(fruits);
+
+fruits.push("pear");
+console.log(fruits);
+
+fruits.reverse();
+console.log(fruits);
