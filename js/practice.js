297c597ce4cdff73f6e583cd5b78117cfc476feb
Jordan Foote <jordan.foote@outlook.com>
2022-03-11 14:19:34 -0600

    Updating

---
+++ js/practice.js
@@ -0,0 +1,15 @@
+"use strict"
+
+var count = 0;
+var max = 10;
+var interval = 1000; // interval time in milliseconds
+
+var intervalId = setInterval(function () {
+    if (count >= max) {
+        clearInterval(intervalId);
+        console.log('All done');
+    } else {
+        count++;
+        console.log('Repeating this line ' + count);
+    }
+}, interval);
