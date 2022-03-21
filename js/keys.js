4804042c256f5619f264d5a3b27c6fb308e0c30c
Jordan Foote <jordan.foote@outlook.com>
2022-03-20 20:13:57 -0500

    Updating Exercises

---
+++ js/keys.js
@@ -0,0 +1,3 @@
+const OWM_KEY = "59ba1fc20fc5a90db4b8be9c85a1f969";
+const SHAZAM_KEY = "3b539606bdmsh96485d9c0950a87p161ffdjsn351cfa8eabb4";
+const MAP_KEY = "pk.eyJ1Ijoiam9yZGFuZm9vdGUiLCJhIjoiY2wwdmQ2aTNzMTVwMTNqcXAxcXJtc2g3YiJ9.XAd8ljzelg_u__IOHQrlSQ";
